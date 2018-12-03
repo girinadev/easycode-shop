a70830d56fa539dd11c3ed3fcb387f3f55c76f2a
Irina Gershun <girinadev@gmail.com>
2018-12-03 21:33:41 +0200

    Implemented tabs

--- src/js/init.js
+++ src/js/init.js
@@ -1,3 +1,7 @@
 $(function () {
-    $(".newestproducts__tabs").tabs();
+    $("#newestproducts-tabs").tabs();
+    $('.newestproducts__tabs-item-link').click(function(){
+        $('.newestproducts__tabs-item-link').removeClass('newestproducts__tabs-item-link--active');
+        $(this).addClass('newestproducts__tabs-item-link--active');
+    });
 });
